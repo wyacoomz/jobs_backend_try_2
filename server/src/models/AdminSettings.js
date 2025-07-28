722f13fc50b4fb0356aeb1f807e1ece77c2dc760
Abdul Samad <abdulischad@gmail.com>
2025-07-28 17:05:42 +0530

    fixed

--- server/src/models/AdminSettings.js
+++ server/src/models/AdminSettings.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const AdminSettingsSchema = new mongoose.Schema(
   {
-    jobPostPrice: { type: Number, default: 20, min: 1 } // price per job post
+    jobPostPrice: { type: Number, default: 20} // price per job post
   },
   { timestamps: true }
 );
