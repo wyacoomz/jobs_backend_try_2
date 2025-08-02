b031d3d1f5f2c4ba4b450963e353eda1f09354f3
wyacoomz <abdulischad@gmail.com>
2025-08-02 16:24:54 +0530

    removed company logo auth.routes.js

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -6,7 +6,7 @@ import { protect } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register/user",   upload.single("resume"), registerUser);
-router.post("/register/recruiter", upload.single("logo"), registerRecruiter);
+router.post("/register/recruiter", registerRecruiter);
 router.post("/login", login)
 router.post("/login/user",      loginUser);
 router.post("/login/recruiter", loginRecruiter);
